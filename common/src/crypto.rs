a87259b5cc4389d9835376e3bd325d6e466e9638
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2026-03-20 17:36:38 +0100

    Draft types and traits for `common` module

---
+++ common/src/crypto.rs
@@ -0,0 +1,51 @@
+use serde::{Deserializer, Serializer};
+use std::fmt::{Debug, Formatter};
+
+pub trait CryptoHash {
+    fn crypto_hash(&self) -> blake3::Hash;
+}
+
+pub struct SignedHash {
+    public_key: aws_lc_rs::signature::ParsedPublicKey,
+    timestamp: usize,
+    signature: aws_lc_rs::signature::Signature,
+}
+
+impl SignedHash {
+    pub fn sign(hash: &blake3::Hash, key_pair: aws_lc_rs::signature::Ed25519KeyPair) -> SignedHash {
+        todo!()
+    }
+
+    pub fn verify(&self, unsigned_hash: &blake3::Hash) -> Result<(), aws_lc_rs::error::KeyRejected> {
+        todo!()
+    }
+}
+
+impl serde::Serialize for SignedHash {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer
+    {
+        todo!()
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for SignedHash {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>
+    {
+        todo!()
+    }
+}
+
+impl Debug for SignedHash {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    compile_error!("todo: unit tests");
+}
---
+++ common/src/path.rs
@@ -0,0 +1,46 @@
+use std::fmt::{Display, Formatter};
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct RepoPath {
+    components: Vec<RepoPathComponent>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct RepoPathComponent {
+    inner: String,
+}
+
+impl Display for RepoPath {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl From<RepoPath> for PathBuf {
+    fn from(value: RepoPath) -> Self {
+        todo!()
+    }
+}
+
+impl TryFrom<&Path> for RepoPath {
+    type Error = ();
+
+    fn try_from(value: &Path) -> Result<Self, Self::Error> {
+        todo!("error handling, e.g. fail if path is absolute or contains ..")
+    }
+}
+
+impl FromStr for RepoPathComponent {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        todo!("validate format")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    compile_error!("todo: unit tests");
+}
