be81d749d866b930d8bb87f9b26c29360f99afc4
Wilmer Fredriksson Handler <wilmerfhandler@gmail.com>
2026-04-02 11:30:36 +0200

    LocalRepo interface draft

--- vcs-core/src/repo.rs
+++ vcs-core/src/repo.rs
@@ -1,5 +1,6 @@
 use crate::commit::{Commit, CommitId};
 use crate::diff::{FileDiff, RepoDiff};
+use crate::path::RepoPath;
 use crate::storage::{LazyStorage, Storage};
 use std::hash::Hash;
 
@@ -17,9 +18,11 @@ pub struct LocalRepo<HashType, S: RepoStorage<HashType>>
 where
     HashType: Hash + Eq + Send + Sync,
 {
+    head: CommitId,
     commits: LazyStorage<CommitId, Commit<HashType>, S>,
     repo_diffs: LazyStorage<HashType, RepoDiff<HashType>, S>,
     file_diffs: LazyStorage<HashType, FileDiff, S>,
+    indicies: LazyStorage<HashType, RepoDiff<HashType>, S>,
 }
 
 impl<HashType, S> LocalRepo<HashType, S>
@@ -35,7 +38,33 @@ where
         self.commits.get(id).await
     }
 
-    // ...
+    pub async fn head_commit(&self) -> &Commit<HashType> {
+        todo!()
+    }
+
+    pub async fn make_commit(&mut self, message: String) {
+        todo!()
+    }
+
+    pub async fn stage(&mut self, repo_path: &RepoPath) {
+        todo!()
+    }
+
+    pub async fn unstage(&mut self, repo_path: &RepoPath) {
+        todo!()
+    }
+
+    pub async fn checkout(&mut self, commit_id: &CommitId) {
+        todo!()
+    }
+
+    async fn transition_diff(&self, from: &CommitId, to: &CommitId) -> RepoDiff<HashType> {
+        todo!()
+    }
+
+    pub fn head(&self) -> &CommitId {
+        &self.head
+    }
 }
 
 #[cfg(test)]
