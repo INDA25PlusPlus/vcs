70c7047a53ab991bd0fb8fee04a133a3b8e01dc3
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2026-04-17 23:23:31 +0200

    implement Revision type. replace Commit with Revision

--- vcs-core/src/repo/repo_storage.rs
+++ vcs-core/src/repo/repo_storage.rs
@@ -1,15 +1,15 @@
-use crate::commit::{CommitHeader, CommitId, CommitMetadata};
 use crate::crypto::CryptoHash;
 use crate::diff::file_diff::FileDiff;
 use crate::diff::repo_diff::RepoDiff;
 use crate::repo::index::Index;
+use crate::revision::{RevisionHeader, RevisionId, RevisionMetadata};
 use crate::storage::Storage;
 use std::error::Error;
 
 pub trait RepoStorage<H: CryptoHash>:
-    Storage<CommitId, CommitHeader<H>, Error = Self::RepoError>
-    + Storage<CommitId, CommitMetadata<H>, Error = Self::RepoError>
-    + Storage<CommitId, Index<H>, Error = Self::RepoError>
+    Storage<RevisionId<H>, RevisionHeader<H>, Error = Self::RepoError>
+    + Storage<RevisionId<H>, RevisionMetadata<H>, Error = Self::RepoError>
+    + Storage<RevisionId<H>, Index<H>, Error = Self::RepoError>
     + Storage<H, RepoDiff<H>, Error = Self::RepoError>
     + Storage<H, FileDiff, Error = Self::RepoError>
     + Send
