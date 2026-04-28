370db5e81555407e8f2ca850a7217c42a4260da4
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2026-04-28 17:36:01 +0200

    update signature of `FileSystem::changed_files`

--- vcs-core/src/fs.rs
+++ vcs-core/src/fs.rs
@@ -1,16 +1,23 @@
 pub mod path;
 
+use crate::diff::repo_diff::RepoDiff;
+use crate::repo::PendingChanges;
 use crate::{
     crypto::digest::{CryptoDigest, CryptoHash},
-    repo::{Repo, repo_storage::RepoStorage},
+    repo::repo_storage::RepoStorage,
 };
 use path::RepoPath;
-use std::{collections::HashSet, error::Error};
+use std::error::Error;
 use std::{future::Future, hash::Hash};
 
 pub struct File {
     content: Box<[u8]>,
     executable: bool,
+}
+
+pub struct FileTree<D: CryptoDigest> {
+    // todo lazy loading from aggregate repo diffs
+    diff: RepoDiff<D>,
 }
 
 pub trait FileSystem<D: CryptoDigest + CryptoHash, S>
@@ -25,5 +32,16 @@ where
 
     fn write(path: &RepoPath, file: &File) -> impl Future<Output = Result<(), Self::Error>> + Send;
 
-    fn changed_files(repo: &Repo<D, S>) -> impl Future<Output = HashSet<RepoPath>> + Send;
+    fn update_pending_changes(
+        head_tree: &FileTree<D>,
+        pending_changes: &mut PendingChanges<D>,
+    ) -> impl Future<Output = Result<(), Self::Error>> + Send;
+}
+
+impl<D: CryptoDigest> TryFrom<RepoDiff<D>> for FileTree<D> {
+    type Error = ();
+
+    fn try_from(value: RepoDiff<D>) -> Result<Self, Self::Error> {
+        todo!("check only file creation etc.")
+    }
 }
