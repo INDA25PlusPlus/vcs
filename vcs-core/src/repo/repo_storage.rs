64bc60b41da4fa556cc2454d3c56ee4f3e9b5cac
Wilmer Fredriksson Handler <wilmerfhandler@gmail.com>
2026-04-28 16:16:51 +0200

    Rename working tree -> pending changes and index -> staged changes

--- vcs-core/src/repo/repo_storage.rs
+++ vcs-core/src/repo/repo_storage.rs
@@ -1,7 +1,7 @@
 use crate::crypto::digest::{CryptoDigest, CryptoHash};
 use crate::diff::file_diff::{FileDiff, FileDiffRef};
 use crate::diff::repo_diff::{RepoDiff, RepoDiffRef};
-use crate::repo::index::Index;
+use crate::repo::{PendingChanges, StagedChanges};
 use crate::revision::{RevisionHeader, RevisionId, RevisionMetadata};
 use crate::storage::Storage;
 use std::error::Error;
@@ -10,7 +10,8 @@ pub trait RepoStorage<D: CryptoDigest + CryptoHash>:
     Storage<(), RevisionId<D>, Error = Self::RepoStorageError>
     + Storage<RevisionId<D>, RevisionHeader<D>, Error = Self::RepoStorageError>
     + Storage<RevisionId<D>, RevisionMetadata<D>, Error = Self::RepoStorageError>
-    + Storage<RevisionId<D>, Index<D>, Error = Self::RepoStorageError>
+    + Storage<RevisionId<D>, PendingChanges<D>, Error = Self::RepoStorageError>
+    + Storage<RevisionId<D>, StagedChanges<D>, Error = Self::RepoStorageError>
     + Storage<RepoDiffRef<D>, RepoDiff<D>, Error = Self::RepoStorageError>
     + Storage<FileDiffRef<D>, FileDiff, Error = Self::RepoStorageError>
     + Send
