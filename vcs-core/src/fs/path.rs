b482882e6aa3a52af4c1b320b47cf2b3456dc001
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2026-04-29 20:53:36 +0200

    `RepoPath` impl

--- vcs-core/src/fs/path.rs
+++ vcs-core/src/fs/path.rs
@@ -2,14 +2,36 @@ use std::fmt::{Display, Formatter};
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
 
+/// Relative path referring to a directory or (regular) file within a repository. Composed of zero
+/// or more [`RepoPathComponent`]s. A path with zero components corresponds to the repository root
+/// directory. Imposes some limitations to ensure compatibility with Linux paths:
+/// - Combined length of path components plus path separators must be less than or equal to 4096.
+///
+/// Limitations for path components:
+/// - Length must be between 1 and 255 bytes, inclusive.
+/// - May not contain the null byte ('\0') or the slash symbol ('/').
+/// - May not be equal to '.' or '..'.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct RepoPath {
-    components: Vec<RepoPathComponent>,
+    components: Box<[RepoPathComponent]>,
 }
 
+/// Component of a [`RepoPath`] corresponding to a directory or file name.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct RepoPathComponent {
-    inner: String,
+    inner: Box<[u8]>,
+}
+
+impl RepoPath {
+    pub fn new() -> RepoPath {
+        RepoPath {
+            components: vec![].into_boxed_slice(),
+        }
+    }
+
+    pub fn components(&self) -> &[RepoPathComponent] {
+        &self.components
+    }
 }
 
 impl Display for RepoPath {
@@ -18,8 +40,10 @@ impl Display for RepoPath {
     }
 }
 
-impl From<RepoPath> for PathBuf {
-    fn from(value: RepoPath) -> Self {
+impl TryFrom<&RepoPath> for PathBuf {
+    type Error = ();
+
+    fn try_from(value: &RepoPath) -> Result<PathBuf, Self::Error> {
         todo!()
     }
 }
