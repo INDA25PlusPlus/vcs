1f8b3e5280fe3be89157b125621dc1dca792906d
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2026-05-05 02:45:09 +0200

    update `FileSystem` interface

--- vcs-core/src/fs.rs
+++ vcs-core/src/fs.rs
@@ -5,11 +5,10 @@ use crate::crypto::digest::{CryptoDigest, CryptoHash};
 use crate::diff::repo_diff::RepoDiff;
 use crate::fs::file::FileRef;
 use crate::repo::PendingChanges;
-use crate::repo::repo_storage::RepoStorage;
+use crate::storage::Storage;
 use file::File;
 use path::RepoPath;
 use std::collections::HashMap;
-use std::error::Error;
 use std::{future::Future, hash::Hash};
 
 pub struct FileTree<D> {
@@ -17,25 +16,68 @@ pub struct FileTree<D> {
     files: HashMap<RepoPath, FileRef<D>>,
 }
 
-pub trait FileSystem<D: CryptoDigest + CryptoHash, S>
+pub type FileSystemResult<T, E> = Result<T, FileSystemError<E>>;
+
+pub enum FileSystemError<E> {
+    InternalError(E),
+    MissingFile,
+}
+
+pub trait FileSystem<D: CryptoDigest + CryptoHash>
 where
     D: Hash + Eq + Send + Sync,
-    S: RepoStorage<D>,
-    S::RepoStorageError: Error + Send,
 {
     type Error;
 
-    fn read(path: &RepoPath) -> impl Future<Output = Result<File, Self::Error>> + Send;
+    /// Read a [`File`] from the file system
+    fn read(
+        &self,
+        path: &RepoPath,
+    ) -> impl Future<Output = FileSystemResult<File, Self::Error>> + Send;
 
-    fn write(path: &RepoPath, file: &File) -> impl Future<Output = Result<(), Self::Error>> + Send;
-
-    fn update_pending_changes(
-        head_tree: &FileTree<D>,
-        pending_changes: &mut PendingChanges<D>,
+    /// Write a [`File`] to the file system
+    fn write(
+        &self,
+        path: &RepoPath,
+        file: &File,
     ) -> impl Future<Output = Result<(), Self::Error>> + Send;
+
+    /// Delete a [`File`] from the file system
+    fn delete(
+        &self,
+        path: &RepoPath,
+    ) -> impl Future<Output = FileSystemResult<(), Self::Error>> + Send;
+
+    /// Update `pending_changes` to match the diff from `head` to the current file tree.
+    /// (`pending_changes` = files - `head`)
+    ///
+    /// `head_changed`: Set to `true` if `head` may have changed since the last call to
+    /// `read_pending_changes` or `write_pending_changes`. If `false`, the implementer may assume
+    /// that `head` has not changed.
+    fn read_pending_changes<S: Storage<D, File>>(
+        &self,
+        storage: &S,
+        head: &FileTree<D>,
+        pending_changes: &mut PendingChanges<D>,
+        head_changed: bool,
+    ) -> impl Future<Output = Result<(), Self::Error>>;
+
+    /// Update the file tree to match `pending_changes` applied to `head`.
+    /// (files = `head` + `pending_changes`)
+    ///
+    /// `head_changed`: Set to `true` if `head` may have changed since the last call to
+    /// `read_pending_changes` or `write_pending_changes`. If `false`, the implementer may assume
+    /// that `head` has not changed.
+    fn write_pending_changes<S: Storage<D, File>>(
+        &self,
+        storage: &S,
+        head: &FileTree<D>,
+        pending_changes: &PendingChanges<D>,
+        head_changed: bool,
+    ) -> impl Future<Output = Result<(), Self::Error>>;
 }
 
-impl<D: CryptoDigest> TryFrom<RepoDiff<D>> for FileTree<D> {
+impl<D: CryptoDigest + CryptoHash> TryFrom<RepoDiff<D>> for FileTree<D> {
     type Error = ();
 
     fn try_from(value: RepoDiff<D>) -> Result<Self, Self::Error> {
