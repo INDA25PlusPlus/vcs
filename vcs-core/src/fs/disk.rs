50f3b37158dad9dd5dc7b9be47dd6e932dd30287
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2026-05-19 01:05:53 +0200

    implement apply_pending_change for DiskFileSystem

--- vcs-core/src/fs/disk.rs
+++ vcs-core/src/fs/disk.rs
@@ -1,5 +1,5 @@
 use crate::changeset::Changeset;
-use crate::changeset::file::{File, FileChange, FileRef};
+use crate::changeset::file::{File, FileChange, FileDiff, FileDiffRef, FileRef};
 use crate::crypto::digest::{CryptoDigest, CryptoHash};
 use crate::diff::diff_policy::DiffPolicy;
 use crate::fs::map_ops::{
@@ -7,8 +7,9 @@ use crate::fs::map_ops::{
 };
 use crate::fs::path::{RepoPath, RepoPathComponent, RepoPathError};
 use crate::fs::{
-    FileSystem, FileSystemError, FileSystemReadError, FileSystemReadResult, FileSystemWriteResult,
-    FileTree, update_create_file_change, update_delete_file_change, update_modify_file_change,
+    FileSystem, FileSystemError, FileSystemReadError, FileSystemReadResult, FileSystemWriteError,
+    FileSystemWriteResult, FileTree, update_create_file_change, update_delete_file_change,
+    update_modify_file_change,
 };
 use crate::repo::PendingChanges;
 use crate::repo::repo_storage::RepoStorage;
@@ -20,14 +21,13 @@ use std::fs::{FileType, Metadata};
 use std::ops::Deref;
 use std::path::{Path, PathBuf};
 use std::time::SystemTime;
-use tokio::sync::Mutex;
 use tokio::try_join;
 
 pub const IGNORED_PATH: &str = ".vcs";
 
 pub struct DiskFileSystem {
     base_path: Box<Path>,
-    cache_times: Mutex<DashMap<RepoPath, SystemTime>>,
+    cache_times: DashMap<RepoPath, SystemTime>,
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
@@ -39,10 +39,15 @@ pub enum Error {
 }
 
 impl DiskFileSystem {
-    fn is_dirty(metadata: &Metadata, cache_time: Option<&SystemTime>) -> Result<bool> {
-        Ok(match cache_time {
-            None => true,
-            Some(cache_time) => &metadata.modified()? > cache_time,
+    fn is_dirty(metadata: Option<&Metadata>, cache_time: Option<&SystemTime>) -> Result<bool> {
+        Ok(match (metadata, cache_time) {
+            // file does not exist on file => dirty
+            (None, _) => true,
+            // file does exist on file but has not been cached => dirty
+            (Some(_), None) => true,
+            // file has been modified since last cache => dirty
+            // file has not been modified since last cache => not dirty
+            (Some(metadata), Some(cache_time)) => &metadata.modified()? > cache_time,
         })
     }
 
@@ -73,8 +78,52 @@ impl DiskFileSystem {
         })
     }
 
-    async fn read_metadata(path: &Path) -> Result<Metadata> {
-        Ok(tokio::fs::metadata(path).await?)
+    async fn write_file_cached_path(
+        cached_path: Option<&Path>,
+        base_path: &Path,
+        repo_path: &RepoPath,
+        file: &File,
+    ) -> Result<()> {
+        match cached_path {
+            None => {
+                // file does not exist in cache
+                let mut fs_path: PathBuf = base_path.into();
+                repo_path
+                    .append_to(&mut fs_path)
+                    .map_err(Error::InvalidPath)?;
+                DiskFileSystem::write_file(&fs_path, file).await?;
+            }
+            Some(fs_path) => {
+                // file does exist in cache
+                DiskFileSystem::write_file(fs_path, file).await?;
+            }
+        }
+        Ok(())
+    }
+
+    async fn write_file(path: &Path, file: &File) -> Result<()> {
+        if let Some(parent) = path.parent() {
+            tokio::fs::create_dir_all(parent).await?;
+        }
+        tokio::fs::write(path, &file.content).await?;
+
+        cfg_if! {
+            if #[cfg(unix)] {
+                use std::os::unix::fs::PermissionsExt;
+                let mut perms = tokio::fs::metadata(path).await?.permissions();
+                if file.executable_status {
+                    perms.set_mode(perms.mode() | 0o111);
+                } else {
+                    perms.set_mode(perms.mode() & 0o666);
+                }
+                tokio::fs::set_permissions(path, perms).await?;
+            }
+        }
+        Ok(())
+    }
+
+    async fn delete_file(path: &Path) -> Result<()> {
+        Ok(tokio::fs::remove_file(path).await?)
     }
 
     async fn index_files_recurse(
@@ -143,15 +192,13 @@ impl FileSystem for DiskFileSystem {
             FileSystemReadError::FileSystemError(FileSystemError::InternalError(err))
         })?;
 
-        let mut cache_times = self.cache_times.lock().await;
-
         let PendingChanges(Changeset { changeset }) = pending_changes;
         let changeset_rw = DashMapGuard::new(changeset);
 
         let now = SystemTime::now();
 
         {
-            let cache_times_ro = DashMapReadOnlyGuard::new(&mut cache_times);
+            let cache_times_ro = DashMapReadOnlyGuard::new(&mut self.cache_times);
 
             // regardless of if head changed, remove all changes to files that don't exist neither on
             // head nor in the file system
@@ -173,7 +220,7 @@ impl FileSystem for DiskFileSystem {
             try_join_all(futures).await?;
         }
         // set all non-dirty
-        cache_times
+        self.cache_times
             .iter_mut()
             .for_each(|mut cache_time| *cache_time = now);
         Ok(())
@@ -190,7 +237,53 @@ impl FileSystem for DiskFileSystem {
         D: CryptoDigest + CryptoHash + Send + Eq,
         S: RepoStorage<D>,
     {
-        todo!()
+        let index = self.index_files(&self.base_path).await.map_err(|err| {
+            FileSystemWriteError::FileSystemError(FileSystemError::InternalError(err))
+        })?;
+
+        let PendingChanges(Changeset { changeset }) = pending_changes;
+
+        // regardless of if head changed, delete all files that don't exist on head and are not
+        // changed in pending changes
+        let remove_difference_futures = index.iter().filter_map(|(path, index_entry)| {
+            let should_delete = !head.files.contains_key(path) && !changeset.contains_key(path);
+            should_delete.then_some(async {
+                DiskFileSystem::delete_file(&index_entry.fs_path)
+                    .await
+                    .map_err(|err| {
+                        FileSystemWriteError::FileSystemError(FileSystemError::InternalError(err))
+                    })
+            })
+        });
+
+        let outer_join = outer_join(&head.files, changeset);
+
+        let now = SystemTime::now();
+        let apply_futures = outer_join.map(|(path, outer_join)| async {
+            let cache_time = self.cache_times.get(path).map(|cache_time| *cache_time);
+
+            apply_change(
+                &self.base_path,
+                storage,
+                index.get(path),
+                cache_time,
+                head_changed,
+                path,
+                outer_join,
+            )
+            .await?;
+
+            self.cache_times.insert(path.clone(), now);
+            Ok(())
+        });
+
+        // futures can be safely run concurrently as they operate on disjoint sets of files
+        try_join!(
+            try_join_all(remove_difference_futures),
+            try_join_all(apply_futures)
+        )?;
+
+        Ok(())
     }
 }
 
@@ -219,7 +312,7 @@ where
             // => the file has been created
 
             // only update if head has changed or the file has been changed
-            let dirty = DiskFileSystem::is_dirty(metadata, cache_time).map_err(|err| {
+            let dirty = DiskFileSystem::is_dirty(Some(metadata), cache_time).map_err(|err| {
                 FileSystemReadError::FileSystemError(FileSystemError::InternalError(err))
             })?;
             if head_changed || dirty {
@@ -236,7 +329,7 @@ where
             // => the file may have been modified
 
             // only update if head has changed or the file has been changed
-            let dirty = DiskFileSystem::is_dirty(metadata, cache_time).map_err(|err| {
+            let dirty = DiskFileSystem::is_dirty(Some(metadata), cache_time).map_err(|err| {
                 FileSystemReadError::FileSystemError(FileSystemError::InternalError(err))
             })?;
             if head_changed || dirty {
@@ -260,6 +353,132 @@ where
             }
         }
     }
+    Ok(())
+}
+
+async fn apply_change<D, S>(
+    base_path: &Path,
+    storage: &S,
+    index_entry: Option<&FileIndexEntry>,
+    cache_time: Option<SystemTime>,
+    head_changed: bool,
+    path: &RepoPath,
+    join: OuterJoinEntry<&D, &FileChange<D>>,
+) -> FileSystemWriteResult<(), Error, S::RepoStorageError>
+where
+    D: CryptoDigest + CryptoHash + Send + Eq,
+    S: RepoStorage<D>,
+{
+    let dirty = DiskFileSystem::is_dirty(
+        index_entry.map(|entry| &entry.metadata),
+        cache_time.as_ref(),
+    )
+    .map_err(|err| FileSystemWriteError::FileSystemError(FileSystemError::InternalError(err)))?;
+    match join {
+        OuterJoinEntry::Left(file_digest)
+        | OuterJoinEntry::Right(FileChange::Create(file_digest)) => {
+            // left: file exists on head, not changed in pending changes
+            // right: file does not exist on head but is created in pending changes
+            // both cases => insert file in file system
+            if head_changed || dirty {
+                insert_file(base_path, storage, index_entry, path, file_digest).await?;
+            }
+        }
+        OuterJoinEntry::Both(on_head_digest, FileChange::Modify(pending_file_diff_digest)) => {
+            if head_changed || dirty {
+                modify_file(
+                    base_path,
+                    storage,
+                    index_entry,
+                    path,
+                    on_head_digest,
+                    pending_file_diff_digest,
+                )
+                .await?;
+            }
+        }
+        OuterJoinEntry::Both(_, FileChange::Delete) => {
+            // file exists on head and is deleted in pending changes
+            // => delete file on file system if present
+            if let Some(index_entry) = index_entry {
+                DiskFileSystem::delete_file(&index_entry.fs_path)
+                    .await
+                    .map_err(|err| {
+                        FileSystemWriteError::FileSystemError(FileSystemError::InternalError(err))
+                    })?;
+            }
+        }
+        OuterJoinEntry::Right(_) | OuterJoinEntry::Both(_, FileChange::Create(_)) => {
+            // right: file does not exist on head but is modified or deleted in pending changes
+            // left and right: file exists on head and is created in pending changes
+            // both cases => invalid
+            return Err(FileSystemWriteError::InvalidPendingChanges);
+        }
+    }
+    Ok(())
+}
+
+async fn insert_file<D, S>(
+    base_path: &Path,
+    storage: &S,
+    index_entry: Option<&FileIndexEntry>,
+    path: &RepoPath,
+    file_digest: &D,
+) -> FileSystemWriteResult<(), Error, S::RepoStorageError>
+where
+    D: CryptoDigest + CryptoHash + Send + Eq,
+    S: RepoStorage<D>,
+{
+    let file = <S as Storage<FileRef<D>, File>>::load(storage, file_digest)
+        .await
+        .map_err(FileSystemWriteError::LoadError)?;
+
+    DiskFileSystem::write_file_cached_path(
+        index_entry.map(|entry| entry.fs_path.as_ref()),
+        base_path,
+        path,
+        &file,
+    )
+    .await
+    .map_err(|err| FileSystemWriteError::FileSystemError(FileSystemError::InternalError(err)))?;
+    Ok(())
+}
+
+async fn modify_file<D, S>(
+    base_path: &Path,
+    storage: &S,
+    index_entry: Option<&FileIndexEntry>,
+    path: &RepoPath,
+    file_before_digest: &FileRef<D>,
+    file_diff_digest: &FileDiffRef<D>,
+) -> FileSystemWriteResult<(), Error, S::RepoStorageError>
+where
+    D: CryptoDigest + CryptoHash + Send + Eq,
+    S: RepoStorage<D>,
+{
+    let (file_before, file_diff) = try_join!(
+        <S as Storage<FileRef<D>, File>>::load(storage, file_before_digest),
+        <S as Storage<FileDiffRef<D>, FileDiff>>::load(storage, file_diff_digest)
+    )
+    .map_err(FileSystemWriteError::LoadError)?;
+
+    let file_after_contents = file_diff
+        .hunks
+        .apply(&file_before.content)
+        .map_err(FileSystemWriteError::HunkError)?;
+    let file_after = File {
+        content: file_after_contents,
+        executable_status: file_diff.executable_status,
+    };
+
+    DiskFileSystem::write_file_cached_path(
+        index_entry.map(|entry| entry.fs_path.as_ref()),
+        base_path,
+        path,
+        &file_after,
+    )
+    .await
+    .map_err(|err| FileSystemWriteError::FileSystemError(FileSystemError::InternalError(err)))?;
     Ok(())
 }
 
