19f55a12e3df22c389bb42e27417bc6cb751efda
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2026-05-18 20:59:52 +0200

    implement DiskStorable for RepoStorage types

--- vcs-storage-impl/src/disk.rs
+++ vcs-storage-impl/src/disk.rs
@@ -2,19 +2,11 @@ use serde::{Deserialize, Serialize};
 use std::path::Path;
 
 use vcs_core::crypto::digest::{CryptoDigest, CryptoHash};
-use vcs_core::diff::repo_diff::{RepoDiff, RepoDiffRef};
-use vcs_core::fs::file::{FileDiff, FileDiffRef};
-use vcs_core::repo::{
-    PendingChanges,
-    StagedChanges,
-};
-use vcs_core::revision::{
-    RevisionHeader,
-    RevisionId,
-    RevisionMetadata,
-};
+use vcs_core::diff::repo_diff::RepoDiff;
+use vcs_core::fs::file::{File, FileDiff};
+use vcs_core::repo::{Head, PendingChanges, StagedChanges};
+use vcs_core::revision::{RevisionHeader, RevisionMetadata};
 use vcs_core::storage::{Storage, StorageError, StorageResult};
-use std::error::Error;
 
 pub struct DiskStorage {
     pub base_path: Box<Path>,
@@ -22,9 +14,7 @@ pub struct DiskStorage {
 
 impl DiskStorage {
     pub fn new(base_path: Box<Path>) -> Self {
-        Self {
-            base_path,
-        }
+        Self { base_path }
     }
 }
 
@@ -43,14 +33,14 @@ impl From<std::io::Error> for DiskStorageError {
 
 impl<K, V> Storage<K, V> for DiskStorage
 where
-    K: CryptoDigest,
+    K: DiskStorageKey,
     V: Serialize + for<'de> Deserialize<'de> + DiskStorable,
 {
     type Error = DiskStorageError;
 
     async fn load(&self, key: &K) -> StorageResult<V, Self::Error> {
         // make path from key
-        let filename = hex::encode(key.bytes());
+        let filename = key.to_file_name();
 
         let mut path = self.base_path.to_path_buf();
         path.push(V::OBJECT_PATH);
@@ -70,7 +60,7 @@ where
 
     async fn store(&self, key: &K, value: &V) -> Result<(), Self::Error> {
         // make path from key
-        let filename = hex::encode(key.bytes());
+        let filename = key.to_file_name();
 
         let mut dir = self.base_path.to_path_buf();
         dir.push(V::OBJECT_PATH);
@@ -92,7 +82,7 @@ where
 
     async fn delete(&self, key: &K) -> Result<(), Self::Error> {
         // make path from key
-        let filename = hex::encode(key.bytes());
+        let filename = key.to_file_name();
 
         let mut path = self.base_path.to_path_buf();
         path.push(V::OBJECT_PATH);
@@ -105,26 +95,44 @@ where
     }
 }
 
+pub trait DiskStorageKey {
+    fn to_file_name(&self) -> impl AsRef<Path>;
+}
+
+impl DiskStorageKey for blake3::Hash {
+    fn to_file_name(&self) -> impl AsRef<Path> {
+        hex::encode(self.bytes())
+    }
+}
+
+impl DiskStorageKey for () {
+    fn to_file_name(&self) -> impl AsRef<Path> {
+        "0"
+    }
+}
+
 pub trait DiskStorable {
     const OBJECT_PATH: &'static str;
 }
 
-pub trait RepoStorage<D: CryptoDigest + CryptoHash>:
-    Storage<(), RevisionId<D>, Error = Self::RepoStorageError>
-    + Storage<RevisionId<D>, RevisionHeader<D>, Error = Self::RepoStorageError>
-    + Storage<RevisionId<D>, RevisionMetadata<D>, Error = Self::RepoStorageError>
-    + Storage<RevisionId<D>, PendingChanges<D>, Error = Self::RepoStorageError>
-    + Storage<RevisionId<D>, StagedChanges<D>, Error = Self::RepoStorageError>
-    + Storage<RepoDiffRef<D>, RepoDiff<D>, Error = Self::RepoStorageError>
-    + Storage<FileDiffRef<D>, FileDiff, Error = Self::RepoStorageError>
-    + Send
-    + Sync
-where
-    D: Send,
-{
-    type RepoStorageError: Error + Send;
+macro_rules! impl_disk_storable {
+    { impl $ty:ident = $path:expr; } => {
+        impl DiskStorable for $ty {
+            const OBJECT_PATH: &'static str = $path;
+        }
+    };
+    { impl $ty:ident<D> = $path:expr; } => {
+        impl<D: CryptoDigest + CryptoHash> DiskStorable for $ty<D> {
+            const OBJECT_PATH: &'static str = $path;
+        }
+    };
 }
 
-impl<D: CryptoDigest + CryptoHash> DiskStorable for RevisionMetadata<D> {
-    const OBJECT_PATH: &'static str = "revision_metadata";
-}
+impl_disk_storable! { impl Head<D> = "head"; }
+impl_disk_storable! { impl RevisionHeader<D> = "rev_header"; }
+impl_disk_storable! { impl RevisionMetadata<D> = "rev_meta"; }
+impl_disk_storable! { impl PendingChanges<D> = "pending"; }
+impl_disk_storable! { impl StagedChanges<D> = "staged"; }
+impl_disk_storable! { impl RepoDiff<D> = "repo_diff"; }
+impl_disk_storable! { impl File = "file_content"; }
+impl_disk_storable! { impl FileDiff = "file_diff"; }
